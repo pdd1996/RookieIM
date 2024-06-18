b7785b7d1338bf76a657d831a48f83e6427960a2
paddy <pdd337816464@163.com>
2024-06-18 18:33:28 +0800

    change mapper path

--- rookie-im-server/src/main/java/com/rookie/im/Application.java
+++ rookie-im-server/src/main/java/com/rookie/im/Application.java
@@ -1,9 +1,11 @@
 package com.rookie.im;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.rookie.im.**.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
