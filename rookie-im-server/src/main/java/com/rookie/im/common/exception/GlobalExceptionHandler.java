381396f7f06520a1862c2065811a18e768b0ff3b
Paddy_Pan <pdd337816464@outlook.com>
2024-06-27 20:54:05 +0800

    代码注释

--- rookie-im-server/src/main/java/com/rookie/im/common/exception/GlobalExceptionHandler.java
+++ rookie-im-server/src/main/java/com/rookie/im/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * RestControllerAdvice 全局异常处理器
+ */
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
