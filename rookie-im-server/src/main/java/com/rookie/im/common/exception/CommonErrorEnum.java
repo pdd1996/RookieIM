f4b3aa2760aee85c1f5212ce520bd33e52496b90
Paddy_Pan <pdd337816464@outlook.com>
2024-06-27 21:34:29 +0800

    编写参数校验validation的异常捕捉

---
+++ rookie-im-server/src/main/java/com/rookie/im/common/exception/CommonErrorEnum.java
@@ -0,0 +1,23 @@
+package com.rookie.im.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum CommonErrorEnum implements ErrorEnum{
+    PARAM_INVALID(-2,"参数校验失败{0}")
+    ;
+
+    private Integer errCode;
+    private String errMsg;
+    @Override
+    public Integer getErrorCode() {
+        return errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return errMsg;
+    }
+}
--- rookie-im-server/src/main/java/com/rookie/im/common/exception/GlobalExceptionHandler.java
+++ rookie-im-server/src/main/java/com/rookie/im/common/exception/GlobalExceptionHandler.java
@@ -3,9 +3,12 @@ package com.rookie.im.common.exception;
 import com.rookie.im.common.domain.resp.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.rookie.im.common.exception.CommonErrorEnum.PARAM_INVALID;
 
 /**
  * RestControllerAdvice 全局异常处理器
@@ -13,6 +16,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ApiResult<?> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        StringBuilder errMsg = new StringBuilder();
+        e.getBindingResult().getFieldErrors()
+                .forEach(x -> errMsg.append(x.getField()).append(x.getDefaultMessage()).append(","));
+
+        String msg = errMsg.toString();
+        return ApiResult.fail(PARAM_INVALID.getErrorCode(), msg.substring(0, msg.length() -1));
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = BusinessException.class)
